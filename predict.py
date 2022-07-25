c0a366e2d609d3e87f720a954c47e77dceca5320
JunX <junchen0102@gmail.com>
2022-07-25 10:43:51 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -27,9 +27,9 @@ def load_and_preprocess_image(path):
 def predict(img_path):
     image = load_and_preprocess_image(img_path)
     result = model.predict(image)
-    print(index2label[np.argmax(result)])
-    e.log_metric("result", index2label[np.argmax(result)])
-
+    resinfo = index2label[np.argmax(result)]
+    e.log_metric("result", resinfo)
+    return resinfo
 
 def test_images():
     data_path= '../cifar10_0/test'
